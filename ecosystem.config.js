9599450de494746199850c959cd7b23386f00966
Elzodxon Sharofaddinov <elzodxonsharofaddinov@Elzodxons-MacBook-Pro.local>
2023-04-18 20:57:42 +0500

    feat: name ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'ayoluchun-landing',
+      name: 'Elzodxon.me',
       exec_mode: 'cluster',
       instances: '1',
       script: './node_modules/nuxt/bin/nuxt.js',
